b8ef0f7ac2cfa49aa09c6b4801e585cd31fca279
seiren <seirengames@gmail.com>
2022-05-04 22:00:56 +0900

    replace with std

--- wing/java/NativeArrayTools.hx
+++ wing/java/NativeArrayTools.hx
@@ -1,17 +1,12 @@
 package wing.java;
 
 import java.NativeArray;
-import haxe.ds.ReadOnlyArray;
+import java.Lib;
 
 class NativeArrayTools {
 	// Must be statically typed with `inline` or `@:generic`.
 	// Otherwise `ClassCastException` is raised.
-	@:generic
-	public static function from<T>(array:ReadOnlyArray<T>):NativeArray<T> {
-		final nativeArray:NativeArray<T> = new NativeArray(array.length);
-		for (i in 0...nativeArray.length) {
-			nativeArray[i] = array[i];
-		}
-		return nativeArray;
+	public static inline function from<T>(array:Array<T>):NativeArray<T> {
+		return Lib.nativeArray(array, true);
 	}
 }
