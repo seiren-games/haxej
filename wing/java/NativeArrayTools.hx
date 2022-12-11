7c8bfbc679ef88ca806791dcd7bf121d67c9d6da
seiren <seirengames@gmail.com>
2022-12-11 17:15:06 +0900

    use ReadOnlyArray

--- wing/java/NativeArrayTools.hx
+++ wing/java/NativeArrayTools.hx
@@ -1,5 +1,6 @@
 package wing.java;
 
+import haxe.ds.ReadOnlyArray;
 import java.Lib;
 import java.NativeArray;
 
@@ -7,7 +8,7 @@ class NativeArrayTools {
 	// Must be statically typed with `inline` or `@:generic`.
 	// Otherwise `ClassCastException` is raised.
 	// TODO: rename from2nativeArray;
-	public static inline function from<T>(array:Array<T>):NativeArray<T> {
+	public static inline function from<T>(array:ReadOnlyArray<T>):NativeArray<T> {
 		return Lib.nativeArray(array, true);
 	}
 }
