dab182363376986ed7f8b56c16698c26f9cf1519
seiren <seirengames@gmail.com>
2022-12-11 17:28:43 +0900

    Enable exitValues

--- wing/java/JavaSys.hx
+++ wing/java/JavaSys.hx
@@ -1,5 +1,6 @@
 package wing.java;
 
+import haxe.ds.ReadOnlyArray;
 import java.NativeArray;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
@@ -11,18 +12,16 @@ class JavaSys {
 	 * Prevent garbled output.  
 	 * - `Sys.command` causes garbled output.
 	 */
-	public static function command(cmd:String, ?args:Array<String>):Int {
-		final commandLine:CommandLine =
-			if (args == null) {
-				final comspec:String = Sys.getEnv("COMSPEC").or("cmd.exe");
-				new CommandLine(comspec)
-				.addArguments(NativeArray.make("/C", cmd));
-			}
-			else {
-				new CommandLine(cmd)
-				.addArguments(NativeArrayTools.from(args));
-			}
+	public static function command(cmd:String, ?args:ReadOnlyArray<String>, ?maybeExitValues:ReadOnlyArray<Int>):Int {
+		final commandLine:CommandLine = if (args == null) {
+			final comspec:String = Sys.getEnv("COMSPEC").or("cmd.exe");
+			new CommandLine(comspec).addArguments(NativeArray.make("/C", cmd));
+		} else {
+			new CommandLine(cmd).addArguments(NativeArrayTools.from(args));
+		}
+		final exitValues:ReadOnlyArray<Int> = maybeExitValues.or([0]);
 		final executor:DefaultExecutor = new DefaultExecutor();
+		executor.setExitValues(NativeArrayTools.from(exitValues));
 		return executor.execute(commandLine);
 	}
 }
