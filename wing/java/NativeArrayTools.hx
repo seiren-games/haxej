9d392d4981d32678e4f083a7a0807b81818f85e8
seiren <seirengames@gmail.com>
2022-12-11 16:49:21 +0900

    add todo

--- wing/java/NativeArrayTools.hx
+++ wing/java/NativeArrayTools.hx
@@ -1,11 +1,12 @@
 package wing.java;
 
-import java.NativeArray;
 import java.Lib;
+import java.NativeArray;
 
 class NativeArrayTools {
 	// Must be statically typed with `inline` or `@:generic`.
 	// Otherwise `ClassCastException` is raised.
+	// TODO: rename from2nativeArray;
 	public static inline function from<T>(array:Array<T>):NativeArray<T> {
 		return Lib.nativeArray(array, true);
 	}
